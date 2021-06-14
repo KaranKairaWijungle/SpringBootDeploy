a25a57d9a2efe3843b95050d365bbb83b48c3803
karan.kaira <karan.kaira@httpcart.com>
2021-06-14 15:24:31 +0530

    war packaging

--- src/main/java/com/eduonix/BestRestApiEver/BestRestApiEverApplication.java
+++ src/main/java/com/eduonix/BestRestApiEver/BestRestApiEverApplication.java
@@ -2,9 +2,16 @@ package com.eduonix.BestRestApiEver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BestRestApiEverApplication {
+public class BestRestApiEverApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(BestRestApiEverApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BestRestApiEverApplication.class, args);
